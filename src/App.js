b3b16356cf5950b4632f65dc180294f34633ecc7
Masud Afsar <masud@afsar.info>
2021-02-01 18:04:23 +0330

    Using Task component in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import {useState} from "react";
+import Task from "./Task/Task";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+    const [tasksState, setTasksState] = useState([
+        {title: 'Create a ToDo list App', done: false},
+        {title: 'Learn about ReactJs', done: true}
+    ])
+
+    const taskToggleHandler = (index) => {
+        const task = {...tasksState[index]};
+        task.done=!task.done;
+        const tasks = [...tasksState];
+        tasks[index] = task;
+        setTasksState(tasks);
+    }
+
+    let tasks = tasksState.map((task, index) => {
+        return (
+            <Task
+                key={index}
+                done={task.done}
+                title={task.title}
+                toggleHandler={() => taskToggleHandler(index)}/>
+        );
+    });
+
+    return (
+        <div className="App">
+            {tasks}
+        </div>
+    );
 }
 
 export default App;
