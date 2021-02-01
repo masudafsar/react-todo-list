fff83d0a9333da5f562006673e73d530554c2647
Masud Afsar <masud@afsar.info>
2021-02-01 19:06:44 +0330

    Implement task input by ref to access the text input

--- src/TaskInput/TaskInput.js
+++ src/TaskInput/TaskInput.js
@@ -1,21 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './TaskInput.css';
 
 const TaskInput = ({onSubmitHandler}) => {
-    const [taskTitleState, setTaskTitleState] = useState('');
-
-    const onInputChangeHandler = (event) => {
-        setTaskTitleState(event.target.value);
-    };
+    let taskInput = React.createRef();
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        onSubmitHandler(taskTitleState);
+        if (taskInput.current.value && taskInput.current.value.trim())
+            onSubmitHandler(taskInput.current.value);
+        taskInput.current.value = '';
     };
 
     return (
         <form className="task-input" onSubmit={onFormSubmit}>
-            <input type="text" className="input" placeholder="Inser a task here..." onChange={onInputChangeHandler}/>
+            <input type="text" className="input" placeholder="Inser a task here..." ref={taskInput}/>
             <button type="submit" className="submit">Add</button>
         </form>
     );
