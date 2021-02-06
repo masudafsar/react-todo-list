fbe1cecc3becc4b4844c41cbce66a3e9a90deebc
Masud Afsar <masud@afsar.info>
2021-02-07 02:50:18 +0330

    Implement hide handle when don't done any task

--- src/CollapsiblePanel/CollapsiblePanel.js
+++ src/CollapsiblePanel/CollapsiblePanel.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import ReactDOMServer from 'react-dom/server';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
 
@@ -7,9 +8,14 @@ import './CollapsiblePanel.scss'
 const CollapsiblePanel = ({children}) => {
     const [status, setStatus] = useState(false);
 
+    const toggleStatusHandler = () => {
+        setStatus(!status);
+    };
+
     let handleMessage = 'Show completed tasks';
     let handleIcon = faCaretDown;
     let contentClass = "content";
+    let content = null;
 
     if (status) {
         handleMessage = 'Hide completed tasks';
@@ -17,22 +23,22 @@ const CollapsiblePanel = ({children}) => {
         contentClass = "content show";
     }
 
-    const toggleStatusHandler = () => {
-        setStatus(!status);
-    };
-
-    return (
-        <div className="collapsible-panel">
-            <button type="button" className="handle" onClick={toggleStatusHandler}>
-                <FontAwesomeIcon icon={handleIcon}/>
-                <span>{handleMessage}</span>
-                <FontAwesomeIcon icon={handleIcon}/>
-            </button>
-            <div className={contentClass}>
-                {children}
+    if (children.length !== 0) {
+        content = (
+            <div className="collapsible-panel">
+                <button type="button" className="handle" onClick={toggleStatusHandler}>
+                    <FontAwesomeIcon icon={handleIcon}/>
+                    <span>{handleMessage}</span>
+                    <FontAwesomeIcon icon={handleIcon}/>
+                </button>
+                <div className={contentClass}>
+                    {children}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    return content;
 };
 
 export default CollapsiblePanel;
