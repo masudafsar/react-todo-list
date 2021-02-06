4f23a758f67485411a8886e4df99109ef566ced0
Masud Afsar <masud@afsar.info>
2021-02-06 16:49:39 +0330

    Implement task by font awesome

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Task.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSquare} from "@fortawesome/free-regular-svg-icons";
+import {faCheckSquare} from "@fortawesome/free-regular-svg-icons";
+import {faTrashAlt} from "@fortawesome/free-regular-svg-icons";
 
 const Task = ({title, done, toggleHandler, removeHandler}) => {
     let taskStatusClass = "task todo";
-    if (done) taskStatusClass = "task done";
+    let taskStatusIcon = faSquare;
+
+    if (done) {
+        taskStatusClass = "task done";
+        taskStatusIcon = faCheckSquare;
+    }
 
     return (
         <div className={taskStatusClass}>
@@ -12,7 +21,7 @@ const Task = ({title, done, toggleHandler, removeHandler}) => {
             </div>
             <div className="task-title">{title}</div>
             <button type="button" className="remove" onClick={removeHandler}>
-                Remove
+                <FontAwesomeIcon icon={faTrashAlt}/>
             </button>
         </div>
     )
