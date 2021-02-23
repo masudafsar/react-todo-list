ad1fc3a6424be28f0ff481606208cb82c261f367
Masud Afsar <masud@afsar.info>
2021-02-24 01:06:31 +0330

    Refactor collapsible panel component as class-based component

--- src/components/CollapsiblePanel/CollapsiblePanel.js
+++ src/components/CollapsiblePanel/CollapsiblePanel.js
@@ -1,47 +1,55 @@
-import React, {useState} from 'react';
-import ReactDOMServer from 'react-dom/server';
+import React, {Component} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
 
 import styleClasses from './CollapsiblePanel.module.scss'
 
-const CollapsiblePanel = ({children}) => {
-    const [status, setStatus] = useState(false);
-
-    const toggleStatusHandler = () => {
-        setStatus(!status);
-    };
-
-    let handleMessage = 'Show completed tasks';
-    let handleIcon = faCaretDown;
-    let contentClass = [styleClasses.Content];
-    let content = null;
-
-    if (status) {
-        handleMessage = 'Hide completed tasks';
-        handleIcon = faCaretUp;
-        contentClass.push(styleClasses.Show);
+class CollapsiblePanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            expand: false
+        };
     }
 
-    if (children.length !== 0) {
-        content = (
-            <div className={styleClasses.CollapsiblePanel}>
-                <button
-                    type="button"
-                    className={styleClasses.Handle}
-                    onClick={toggleStatusHandler}>
-                    <FontAwesomeIcon icon={handleIcon}/>
-                    <span>{handleMessage}</span>
-                    <FontAwesomeIcon icon={handleIcon}/>
-                </button>
-                <div className={contentClass.join(' ')}>
-                    {children}
+    render() {
+        let handleMessage = 'Show completed tasks';
+        let handleIcon = faCaretDown;
+        let contentClass = [styleClasses.Content];
+        let content = null;
+
+        if (this.state.expand) {
+            handleMessage = 'Hide completed tasks';
+            handleIcon = faCaretUp;
+            contentClass.push(styleClasses.Show);
+        }
+
+        if (this.props.children.length !== 0) {
+            content = (
+                <div className={styleClasses.CollapsiblePanel}>
+                    <button
+                        type="button"
+                        className={styleClasses.Handle}
+                        onClick={this.toggleStatusHandler}>
+                        <FontAwesomeIcon icon={handleIcon}/>
+                        <span>{handleMessage}</span>
+                        <FontAwesomeIcon icon={handleIcon}/>
+                    </button>
+                    <div className={contentClass.join(' ')}>
+                        {this.props.children}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
+
+        return content;
     }
 
-    return content;
-};
+    toggleStatusHandler() {
+        this.setState((prevState, props) => {
+            return {expand: !prevState.expand};
+        });
+    };
+}
 
 export default CollapsiblePanel;
