587daf6df3c89a98249dfa46019b0998356d1e41
Masud Afsar <masud@afsar.info>
2021-02-24 01:06:05 +0330

    Refactor app component as class-based component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,62 +1,68 @@
-import {useState} from "react";
+import React, {Component} from "react";
 import Task from "../../components/Task/Task";
 import TaskInput from "../../components/TaskInput/TaskInput";
-import CollapsiblePanel
-    from "../../components/CollapsiblePanel/CollapsiblePanel";
+import CollapsiblePanel from "../../components/CollapsiblePanel/CollapsiblePanel";
 
 import styleClasses from './App.module.scss';
 
-const App = () => {
-    const [tasksState, setTasksState] = useState([])
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+        };
+    }
 
-    const taskToggleHandler = (id) => {
-        const index = tasksState.findIndex(task => task.id === id);
-        const task = {...tasksState[index]};
+    render() {
+        let todoTasks = this.state.tasks.filter(task => !task.done).map(this.taskGenerator);
+        let doneTasks = this.state.tasks.filter(task => task.done).map(this.taskGenerator);
+
+        return (
+            <div className={styleClasses.App}>
+                <TaskInput onSubmitHandler={this.addNewTask}/>
+                {todoTasks}
+                <CollapsiblePanel>
+                    {doneTasks}
+                </CollapsiblePanel>
+            </div>
+        );
+    }
+
+    taskToggleHandler = (id) => {
+        const index = this.state.tasks.findIndex(task => task.id === id);
+        const task = {...this.state.tasks[index]};
         task.done = !task.done;
-        const tasks = [...tasksState];
+        const tasks = [...this.state.tasks];
         tasks[index] = task;
-        setTasksState(tasks);
+        this.setState({tasks: tasks});
     }
 
-    const taskRemoveHandler = (id) => {
-        const index = tasksState.findIndex(task => task.id === id);
-        const tasks = [...tasksState];
+    taskRemoveHandler = (id) => {
+        const index = this.state.tasks.findIndex(task => task.id === id);
+        const tasks = [...this.state.tasks];
         tasks.splice(index, 1);
-        setTasksState(tasks);
+        this.setState({tasks: tasks});
     }
 
-    const addNewTask = (taskTitle) => {
-        const tasks = [...tasksState];
+    addNewTask = (taskTitle) => {
+        const tasks = [...this.state.tasks];
         const newTaskId = tasks.length !== 0
             ? tasks[0].id + 1
             : 0;
         tasks.unshift({id: newTaskId, title: taskTitle, done: false});
-        setTasksState(tasks);
+        this.setState({tasks: tasks});
     };
 
-    const taskGenerator = (task) => {
+    taskGenerator = (task) => {
         return (
             <Task
                 key={task.id}
                 done={task.done}
                 title={task.title}
-                toggleHandler={() => taskToggleHandler(task.id)}
-                removeHandler={() => taskRemoveHandler(task.id)}/>
+                toggleHandler={() => this.taskToggleHandler(task.id)}
+                removeHandler={() => this.taskRemoveHandler(task.id)}/>
         );
     };
-
-    let todoTasks = tasksState.filter(task => !task.done).map(taskGenerator);
-    let doneTasks = tasksState.filter(task => task.done).map(taskGenerator);
-
-    return (
-        <div className={styleClasses.App}>
-            <TaskInput onSubmitHandler={addNewTask}/>
-            {todoTasks}
-            <CollapsiblePanel>
-                {doneTasks}
-            </CollapsiblePanel>
-        </div>
-    );
 }
 
 export default App;
